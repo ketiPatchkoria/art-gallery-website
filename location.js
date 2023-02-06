49fc87735602986606604c9df47ab10b06f59426
Keti Patchkoria <ketii.pachkoria@gmail.com>
2023-02-06 23:25:49 +0400

    initial commit

---
+++ location.js
@@ -0,0 +1,4 @@
+const leftArrowDiv = document.querySelector('.left-arrow-icon-div');
+leftArrowDiv.addEventListener('click', function(){
+    window.location.href="index.html";
+})
---
+++ script.js
@@ -0,0 +1,5 @@
+const rightArrowDiv = document.querySelector('.right-arrow-icon-div');
+
+rightArrowDiv.addEventListener('click', function(){
+    window.location.href="location.html";
+})
